92e646537c198d9c6f513d6aacf294abdc6897db
Daniel Thompson <DanThomp507@gmail.com>
2020-09-12 11:10:34 -0400

    fix: installed further dependencies and updated data fetching

--- src/services/get-posts.js
+++ src/services/get-posts.js
@@ -2,15 +2,11 @@ import { gql } from "apollo-boost";
 export const getPosts = gql`
 {
   allPosts(count: 100) {
-    id
-    title
-    body
-    author {
       id
-      firstName
-      lastName
-      email
-    }
+      title
+      body
+      published
+      createdAt
     likelyTopics{
       label
       likelihood
